b58c47bd9a872fad61d2eddccc29b2e71d2f97fe
Jason Hinton <jason_hinton@att.net>
2020-10-02 11:51:49 -0400

    searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +12,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = (len(arr) - 1)
+    mid = 0
+
+    while start <= end:
+        mid = (end + start) // 2
+        if target < arr[mid]:
+            end = mid - 1
+        elif target > arr[mid]:
+            start = mid + 1
+        else:
+            return mid
 
 
     return -1  # not found
